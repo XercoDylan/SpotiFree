47f041e7fffb5e7a3f4b84dc3c1e063c798448f2
XercoDylan <xerco@mit.edu>
2025-08-14 01:43:49 -0400

    Progress 2

--- static/songs.js
+++ static/songs.js
@@ -49,8 +49,10 @@ class Song {
     }
 
     connect_button() {
+        console.log("CONNECT " + this.id)
         const button = document.getElementById(this.id);
         button.addEventListener("click", () => {
+            console.log("CLICK " + this.id)
             this.play()
         
         });
@@ -61,6 +63,7 @@ class Song {
 
 function renderSongs() {
     const songsContainer = document.getElementById("songsContainer");
+    console.log(songs)
     for (const song_id in songs) {
         const cardHTML = `<article class="card"><div class="cover"><img  src=${songs[song_id].image} alt="Album" width="150" height="150"></div><button name="song_id" id="${song_id}" class="play" aria-label="Play"><svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path d="M8 5v14l11-7L8 5z"/></svg></button><div class="meta"><h4 class="title ellip">${songs[song_id].songName}</h4><p class="artist ellip">${songs[song_id].artist}</p></div></article>`
         songsContainer.innerHTML += cardHTML;
